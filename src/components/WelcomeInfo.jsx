7aeada17ca76928463faa42a789aaec7cb8d5315
MattyCi <mjciaravino@gmail.com>
2026-02-13 22:59:01 -0500

    add welcome info

---
+++ src/components/WelcomeInfo.jsx
@@ -0,0 +1,46 @@
+export default function WelcomeInfo() {
+  return (
+        <div class="row text-center">
+            <div class="col-lg-4 bg-light border pt-4 mt-lg-0">
+                <img
+                    src="/src/assets/img/trophy.png"
+                    alt="Trophy" width="140" height="140" />
+                <p class="h1 pt-1">Compete</p>
+                <p>
+                    One game isn't enough to determine who is the best. A true champion can
+                    only be proven throughout an entire season of games. Season GG allows you to track scores over
+                    an entire season, so you can finally prove to your friends that you're better than them.
+                </p>
+            </div>
+            
+            <div class="col-lg-4 bg-light border pt-4 mt-4 mt-lg-0">
+                <img class="rounded-circle"
+                    src="/src/assets/img/question.png"
+                    alt="Question Mark" width="140" height="140" />
+                <p class="h1 pt-1">How does it Work?</p>
+                <p>
+                    Create a season to get started for free, determine when the season will end,
+                    and choose a game to play for the duration of the competition.
+                </p>
+                <p>
+                    Track the results of each round you play every time your friends get together to play.
+                    The better you play, the more points are awarded to you for the round.
+                </p>
+                <p>
+                    At the end of the season, whoever has scored the most points (on average) becomes the 
+                    season champion.
+                </p>
+            </div>
+            
+            <div class="col-lg-4 bg-light border pt-4 mt-4 mt-lg-0">
+                <img src="/src/assets/img/power-button.png" alt="Power Button" width="140" height="140" />
+                <p class="h1 pt-1">Get Started</p>
+                <p>
+                    To get started, create a free account below. Once registered, you can create your 
+                    first season.
+                </p>
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import Carousel from "../components/Carousel";
 import HomeLogo from "../components/HomeLogo";
+import WelcomeInfo from "../components/WelcomeInfo";
 
 export default function Home() {
   return (
     <div className="container">
       <HomeLogo />
       <Carousel />
-      <div className="row my-4">
-        <h1>Home</h1>
-        <p>Welcome to the SeasonGG UI starter with React + Bootstrap + Router.</p>
-      </div>
+      <WelcomeInfo />
     </div>
   )
 }
