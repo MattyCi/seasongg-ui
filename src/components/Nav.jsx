e53ba43a1bc53e58093d95740ed521f96b19e5e4
MattyCi <mjciaravino@gmail.com>
2026-06-09 21:19:28 -0400

    remove unneeded import

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 
 export default function Nav() {
-  const navigate = useNavigate()
   const { isAuthenticated, logout } = useAuth()
 
   const handleLogout = () => {
